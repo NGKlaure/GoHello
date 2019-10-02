e354700968cea3ea4feabf2eac2e6fcec801db7e
gadjou <nadinegadjou@gmail.com>
2019-10-02 00:39:31 -0500

    added array

--- main.go
+++ main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello, 世界")
+	var locale, greeting string
+	var languages = [4]string{"en", "es", "de", "fr"}
+	locale = languages[1]
+/*
+	if locale == "en" {
+		greeting = "Hello"
+	} else if locale == "es" {
+		greeting = "Hola"
+	} else if locale == "de" {
+		greeting = "Guten Tag"
+	} else {
+		greeting = "Yo"
+	}
+*/
+
+switch locale {
+case "en":
+	greeting = "Hello"
+case "es":
+	greeting = "Hola"
+case "de":
+	greeting = "Guten Tag"
+case "fr":
+	greeting = "Bonjour"
+default:
+	greeting = "Yo"
+}
+
+  fmt.Println(greeting + ",Go!/n" )
+  fmt.Printf(greeting + ", Go!\n")
 }
