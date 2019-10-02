ae192e0b2cdb9688136860b4836220723a867255
gadjou <nadinegadjou@gmail.com>
2019-10-02 11:45:09 -0500

    map method added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 pracice hello go app
 to run:
 go run main.go
+map method added
--- main.go
+++ main.go
@@ -1,39 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	var locale, greeting string
+	var locale string
+	var translations = make(map[string]string)
+	translations["en"] = "hello"
+	translations["de"] = "gtten tag"
+	translations["fr"] = "bonjour"
 	//var languages = [4]string{"en", "es", "de", "fr"}
 	//locale = languages[1]
 
-	fmt.Println("enter language code")
-	fmt.Scanf("%s", &locale)
-	/*
-		if locale == "en" {
-			greeting = "Hello"
-		} else if locale == "es" {
-			greeting = "Hola"
-		} else if locale == "de" {
-			greeting = "Guten Tag"
-		} else {
-			greeting = "Yo"
-		}
-	*/
-
-	switch locale {
-	case "en":
-		greeting = "Hello"
-	case "es":
-		greeting = "Hola"
-	case "de":
-		greeting = "Guten Tag"
-	case "fr":
-		greeting = "Bonjour"
-	default:
-		greeting = "Yo"
+	if len(os.Args) == 1 {
+		fmt.Println("enter language code")
+		fmt.Scanf("%s", &locale)
+	} else {
+		locale = os.Args[1]
+	}
+	output := translations[locale]
+	if output == "" {
+		output = "Yo"
 	}
 
-	fmt.Println(greeting + ",Go!/n")
-	fmt.Printf(greeting + ", Go!\n")
+	fmt.Printf(output, "Go")
 }
